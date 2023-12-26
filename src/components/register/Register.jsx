378ea5d284abe6dda733191d39b13ccd09553c96
Thib-numberX <peronnothibault@hotmail.fr>
2023-12-26 20:26:38 +0100

    add new user ok, with message of confirmation and redirection

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,11 +1,69 @@
-import { useState } from "react";
 import "./register.scss";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import RegisterService from "../../services/register.service.js";
 
 function RegisterPage() {
-  const [registerName, setRegisterName] = useState("");
+  const registerService = new RegisterService();
+  const [registerFirstname, setRegisterFirstname] = useState("");
   const [registerLastname, setRegisterLastname] = useState("");
   const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setPasswsord] = useState("");
+  const [registerError, setRegisterError] = useState([]);
+  const [validationMessage, setValidationMessage] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChangeRegisterFields = (value, name) => {
+    if (name == "firstname") {
+      setRegisterFirstname(value);
+    } else if (name == "lastname") {
+      setRegisterLastname(value);
+    } else if (name == "email") {
+      setRegisterEmail(value);
+    } else if (name == "password") {
+      setPasswsord(value);
+    }
+  };
+
+  const handleSubmitRegister = async (evt) => {
+    evt.preventDefault();
+    const newUser = {
+      firstname: registerFirstname,
+      lastname: registerLastname,
+      identifiant: registerEmail,
+      password: registerPassword,
+    };
+    try {
+      const newUserRegitered = await registerService.newUser(newUser);
+      console.log(newUserRegitered);
+      if (newUserRegitered.status == 500) {
+        newUserRegitered.data.error.details.forEach((error) => {
+          console.log(error.message);
+          setRegisterError([...registerError, error.message]);
+          console.log(registerError);
+        });
+        console.log('length', registerError.length);
+        if (newUserRegitered.data.error.details.length > 0) {
+          console.log("condition error");
+          setShowMessage(true);
+          setTimeout(() => {
+            setShowMessage(false), setRegisterError([]);
+          }, 5000);
+        }
+      }else if (newUserRegitered.status == 201){
+        setValidationMessage(newUserRegitered.data.message)
+        setShowMessage(true);
+          setTimeout(() => {
+            setShowMessage(false);
+            navigate("/login");
+          }, 5000);
+      }
+    } catch (error) {
+      console.log("error", error);
+      return error
+    }
+  };
 
   return (
     <section>
@@ -15,15 +73,26 @@ function RegisterPage() {
         </div>
         <div className="register_h2_form">
           <h2>Création de compte</h2>
-          <form className="register_form">
+          {registerError.map((message) => {
+            console.log("map", message, showMessage);
+            return (
+              <p className={showMessage ? "displayErrorMessage" : "none"}>
+                {message}
+              </p>
+            );
+          })}
+          <p className={showMessage ? "displayValidatedMessage" : "none"}>
+                {validationMessage}
+              </p>
+          <form className="register_form" onSubmit={handleSubmitRegister}>
             <label htmlFor="name">Nom</label>
             <input
               type="text"
               id="name"
               placeholder="ton nom"
-              value={registerName}
+              value={registerFirstname}
               onChange={(evt) => {
-                console.log("register name");
+                handleChangeRegisterFields(evt.target.value, "firstname");
               }}
             />
             <label htmlFor="lastname">Prénom</label>
@@ -33,17 +102,17 @@ function RegisterPage() {
               placeholder="ton prénom"
               value={registerLastname}
               onChange={(evt) => {
-                console.log("register lastname");
+                handleChangeRegisterFields(evt.target.value, "lastname");
               }}
             />
-            <label htmlFor="email">Prénom</label>
+            <label htmlFor="email">Identifiant (email)</label>
             <input
               type="email"
               id="email"
               placeholder="exemple@gmail.com"
               value={registerEmail}
               onChange={(evt) => {
-                console.log("register email");
+                handleChangeRegisterFields(evt.target.value, "email");
               }}
             />
             <label htmlFor="password">Mot de passe</label>
@@ -51,13 +120,15 @@ function RegisterPage() {
               type="password"
               id="password"
               placeholder="******"
-              value={registerEmail}
+              value={registerPassword}
               onChange={(evt) => {
-                console.log("register email");
+                handleChangeRegisterFields(evt.target.value, "password");
               }}
             />
             <button type="submit">Créer mon compte</button>
-            <Link to='/login'><p>Se connecter</p></Link>
+            <Link to="/login">
+              <p>Se connecter</p>
+            </Link>
           </form>
         </div>
         <div className="login_footer">
---
+++ src/services/register.service.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:3000/api/",
+    withCredentials: true,
+    credentials: 'include'
+});
+
+class RegisterService {
+    async newUser(userDatas){
+        const axiosResponse = await axiosInstance.post("auth/register", userDatas)
+        .then((response)=>{
+            console.log(response);
+            return response;
+        })
+        .catch((error)=>{
+            console.log(error);
+            return error.response
+        })
+        return axiosResponse;
+    }
+}
+
+export default RegisterService
